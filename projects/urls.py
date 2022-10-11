00bba43979a4e7c1ed7ebc7aeef1d860a83ba584
Sequence <sequence@myrealm.ca>
2022-10-11 10:32:13 +0200

    Added new section, fixed a bug related to category URLs.

--- projects/urls.py
+++ projects/urls.py
@@ -6,7 +6,8 @@ from . import views
 urlpatterns = [
     path("", views.project_index, name="project_index"),
     path("projects/<str:slug>/", views.project_detail, name="project_detail"),
-    path("<str:category>/", views.project_index, name="project_category"),
+    path("category/<str:category>/", views.project_index,
+         name="project_category"),
     path("tech/<str:technology>/", views.project_index,
          name="project_technology"),
 ]
