3e1ec03fefed839bf2d7a49e417baa9c7b3e679a
Sequence <sequence@myrealm.ca>
2022-10-15 14:47:10 +0200

    Updated TinyMCE configuration.

--- dev_portfolio/settings.py
+++ dev_portfolio/settings.py
@@ -130,6 +130,21 @@ EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+TINYMCE_DEFAULT_CONFIG = {
+    "height": "320px",
+    "width": "960px",
+    "menubar": "file edit view insert format tools table help",
+    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
+    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
+    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
+    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
+    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
+    "a11ycheck ltr rtl | showcomments addcomment code",
+    "custom_undo_redo_levels": 10,
+    "language": "en_US",  # To force a specific language instead of the Django current language.
+}
+
 # TINYMCE_JS_URL = STATIC_URL + 'tinymce/tinymce.min.js'
 
 # LOGGING = {
